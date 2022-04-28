30cda1b0cbffad8867dcaacb82deb372d14fb386
EvaMalinina <tetiana.dotsenko@gmail.com>
2022-04-28 14:45:06 +0700

    Updated App.js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return <div>
-        <h1>heyy</h1>
+        <h1>heyy there !</h1>
         <hr/>
         <MarketingApp/>
     </div>
